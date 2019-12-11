69058fb84f3f2b20854d8c5b9405e644953fa70f
adrianalvarado <adrianalvaradomantilla@gmail.com>
2019-12-11 17:39:33 -0600

    Se crearon los componentes footer y header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuheaderComponent } from './menuheader/menuheader.component';
 import { FooterComponent } from './footer/footer.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
