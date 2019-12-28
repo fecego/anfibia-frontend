ab15a19ea65959939b8e94b18a847b8bb7154e4a
karladiaz <dkarla404@gmail.com>
2019-12-27 18:41:35 -0600

    Componente sobre nosotros responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,11 @@ import { MapaSitioComponent } from './mapa-sitio/mapa-sitio.component';
 import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
 import { FishingComponent } from './fishing/fishing.component';
 import { HuntingComponent } from './hunting/hunting.component';
-<<<<<<< HEAD
-import { PreguntasFrecuentesComponent } from './preguntas-frecuentes/preguntas-frecuentes.component';
-import { NosotrosComponent } from './nosotros/nosotros.component';
-=======
 import { SurvivalComponent } from './survival/survival.component';
 import { AsuntosLegalesComponent } from './asuntos-legales/asuntos-legales.component';
 import { UbicacionComponent } from './ubicacion/ubicacion.component';
-import { FormsModule } from '@angular/forms';
-import { PreguntasFrecuentesComponent} from './preguntas-frecuentes/preguntas-frecuentes.component';
->>>>>>> 59f6c96acc5b7fb3b44c0eeac8e8342b19c3f262
+import { PreguntasFrecuentesComponent } from './preguntas-frecuentes/preguntas-frecuentes.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
