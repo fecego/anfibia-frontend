7ae508136a4fb1b3ad6057309f21a8d744ace4d9
adrianalvarado <adrianalvaradomantilla@gmail.com>
2019-12-30 08:55:52 -0600

    Se hicieron cambios en Nosotros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuheaderComponent } from './menuheader/menuheader.component';
@@ -11,20 +10,14 @@ import { MapaSitioComponent } from './mapa-sitio/mapa-sitio.component';
 import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
 import { FishingComponent } from './fishing/fishing.component';
 import { HuntingComponent } from './hunting/hunting.component';
-<<<<<<< HEAD
 import { PreguntasFrecuentesComponent } from './preguntas-frecuentes/preguntas-frecuentes.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
-=======
 import { SurvivalComponent } from './survival/survival.component';
 import { AsuntosLegalesComponent } from './asuntos-legales/asuntos-legales.component';
 import { UbicacionComponent } from './ubicacion/ubicacion.component';
 import { FormsModule } from '@angular/forms';
-import { PreguntasFrecuentesComponent} from './preguntas-frecuentes/preguntas-frecuentes.component';
-<<<<<<< HEAD
 import { Blogv1Component } from './blogv1/blogv1.component';
-=======
->>>>>>> 59f6c96acc5b7fb3b44c0eeac8e8342b19c3f262
->>>>>>> 5003572e323f6b8303c496d7409ff766faf9f4ae
+
 
 @NgModule({
   declarations: [
@@ -40,11 +33,8 @@ import { Blogv1Component } from './blogv1/blogv1.component';
     UbicacionComponent,
     CrearCuentaComponent,
     PreguntasFrecuentesComponent,
-<<<<<<< HEAD
-    Blogv1Component
-=======
+    Blogv1Component,
     NosotrosComponent
->>>>>>> 5003572e323f6b8303c496d7409ff766faf9f4ae
   ],
   imports: [
     BrowserModule,
--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Granim from 'granim';
 
 @Component({
   selector: 'app-nosotros',
@@ -10,6 +11,20 @@ export class NosotrosComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var granimInstance = new Granim({
+      element: '#canvas-basic',
+      direction: 'diagonal',
+      isPausedWhenNotInView: true,
+      states : {
+          "default-state": {
+              gradients: [
+                  ['#000', '#000'],
+                  ['#000', '#000'],
+                  ['#000', '#000']
+              ]
+          }
+      }
+  });
   }
 
 }
